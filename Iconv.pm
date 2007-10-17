43a85434a7e845d4abc1639b846e28b8f50ebe5b
Michael Piotrowski <mxp@dynalabs.de>
2007-10-17 14:18:03 +0000

    Import of MPIOTR/Text-Iconv-1.7 from CPAN.
    gitpan-cpan-distribution: Text-Iconv
    gitpan-cpan-version:      1.7
    gitpan-cpan-path:         MPIOTR/Text-Iconv-1.7.tar.gz
    gitpan-cpan-author:       MPIOTR
    gitpan-cpan-maturity:     released

--- Iconv.pm
+++ Iconv.pm
@@ -1,5 +1,5 @@
 package Text::Iconv;
-# @(#) $Id: Iconv.pm,v 1.9 2007/10/17 14:06:22 mxp Exp $
+# @(#) $Id: Iconv.pm,v 1.10 2007/10/17 14:14:22 mxp Exp $
 # Copyright (c) 2007 Michael Piotrowski
 
 use strict;
@@ -16,7 +16,7 @@ require AutoLoader;
 @EXPORT_OK = qw(
 	convert
 );
-$VERSION = '1.6.1';
+$VERSION = '1.7';
 
 bootstrap Text::Iconv $VERSION;
 
